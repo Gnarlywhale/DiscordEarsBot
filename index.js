8dd54b2c72383fa40ddefb619303f7f2ad0efb3b
Gnarlywhale <trd@ualberta.ca>
2021-05-02 19:46:13 -0600

    da cleanest socket

--- index.js
+++ index.js
@@ -3,7 +3,8 @@
 const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
-const io = socketIO(server);
+
+
 
 const server = express()
   .use(express.static(path.join(__dirname, 'public')))
@@ -11,11 +12,14 @@ const server = express()
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
-
+  
+  const io = require('socket.io')(server);
   io.on('connection', (socket) => {
     console.log('Client connected');
     socket.on('disconnect', () => console.log('Client disconnected'));
   });
+
+  setInterval(() => io.emit('time', new Date().toTimeString()), 1000);
 //////////////////////////////////////////
 //////////////// LOGGING /////////////////
 //////////////////////////////////////////
