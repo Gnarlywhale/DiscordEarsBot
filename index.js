b0f7ed5a2c6034581b94a70a727bf5bc18d92e30
Gnarlywhale <trd@ualberta.ca>
2021-05-02 19:53:48 -0600

    path boys

--- index.js
+++ index.js
@@ -3,16 +3,20 @@
 const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
-
-
+const INDEX = 'public/index.html';
 
 const server = express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
-  
+  .use((req, res) => res.sendFile(INDEX, { root: __dirname }))
+  .listen(PORT, () => console.log(`Listening on ${PORT}`));
+
+
+// const server = express()
+//   .use(express.static(path.join(__dirname, 'public')))
+//   .set('views', path.join(__dirname, 'views'))
+//   .set('view engine', 'ejs')
+//   .get('/', (req, res) => res.render('pages/index'))
+//   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+
   const io = require('socket.io')(server);
   io.on('connection', (socket) => {
     console.log('Client connected');
