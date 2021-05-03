9ada85ce1127c6ee37b29688cd1dec7aadd03e8c
Gnarlywhale <trd@ualberta.ca>
2021-05-02 19:19:12 -0600

    socket test

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
 const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
-const io = require('socket.io')(https);
+const io = require('socket.io')();
 
 express()
   .use(express.static(path.join(__dirname, 'public')))
